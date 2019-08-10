98680d5f68c58bc474ff23ebc0e0661137f8c4fd
SOOSOOC <1805790572@qq.com>
2019-08-10 17:50:37 +0800

    底层数据

--- GameMaster/RPGTale/Assets/Sim_FrameWork/Data/MetaData/TalentsMetaData.cs
+++ GameMaster/RPGTale/Assets/Sim_FrameWork/Data/MetaData/TalentsMetaData.cs
@@ -6,30 +6,216 @@ using System.Xml.Serialization;
 [System.Serializable]
 public class TalentsMetaData : ExcelBase {
 
+    public override void Construction()
+    {
+        AllTalentsList = new List<Talents>();
+        for(int i = 0; i < 2; i++)
+        {
+            Talents ta = new Talents();
+            ta.TalentID = i;
+            ta.TalentType = (ushort)i;
+            ta.TalentName = "";
+            ta.TalentDesc = "";
+            ta.TalentIcon = "";
+            ta.LevelBase = (ushort)i;
+            ta.LevelMax = (ushort)i;
+            ta.TalentRarity = (ushort)i;
+            ta.SkillBaseList = "";
+            ta.SkillExtendParam = "";
+            ta.TalentTagList = "";
+            AllTalentsList.Add(ta);
+        }
 
+        AllTalentTraitDataList = new List<TalentTraitData>();
+        for(int i = 0; i < 2; i++)
+        {
+            TalentTraitData tt = new TalentTraitData();
+            tt.TraitID = i;
+            tt.TraitName = "";
+            tt.TraitDesc = "";
+            tt.TraitIcon = "";
+            tt.TraitEffect = "";
+            AllTalentTraitDataList.Add(tt);
+        }
+
+        AllTalentSkillDataList = new List<TalentSkillData>();
+        for(int i = 0; i < 2; i++)
+        {
+            TalentSkillData ts = new TalentSkillData();
+            ts.SkillID = i;
+            ts.MaxLevel = (ushort)i;
+            ts.SkillRarity = (ushort)i;
+            ts.SkillName = "";
+            ts.SkillDesc = "";
+            ts.SkillIcon = "";
+            ts.SkillEffect = "";
+            ts.SkillGrowth = i;
+            AllTalentSkillDataList.Add(ts);
+        }
+
+        AllTalentTypeDataList = new List<TalentTypeData>();
+        for(int i = 0; i < 2; i++)
+        {
+            TalentTypeData ttype = new TalentTypeData();
+            ttype.Type = "";
+            ttype.TypeName = "";
+            ttype.TypeDesc = "";
+            ttype.TypeIcon = "";
+            AllTalentTypeDataList.Add(ttype);
+        }
+    }
+
+
+    public override void Init()
+    {
+        AllTalentSkillDataDic.Clear();
+        AllTalentTraitDataDic.Clear();
+        AllTalentTypeDataDic.Clear();
+        AllTalentsDic.Clear();
+
+        foreach (var data in AllTalentsList)
+        {
+            if (AllTalentsDic.ContainsKey(data.TalentID))
+            {
+                Debug.LogError("Find Same TalentID , TalentID= " + data.TalentID);
+            }
+            else
+            {
+                AllTalentsDic.Add(data.TalentID, data);
+            }
+        }
+
+        foreach (var data in AllTalentTraitDataList)
+        {
+            if (AllTalentTraitDataDic.ContainsKey(data.TraitID))
+            {
+                Debug.LogError("Find Same TraitID , TraitID= " + data.TraitID);
+            }
+            else
+            {
+                AllTalentTraitDataDic.Add(data.TraitID, data);
+            }
+        }
+
+        foreach (var data in AllTalentSkillDataList)
+        {
+            if (AllTalentSkillDataDic.ContainsKey(data.SkillID))
+            {
+                Debug.LogError("Find Same SkillID , SkillID= " + data.SkillID);
+            }
+            else
+            {
+                AllTalentSkillDataDic.Add(data.SkillID, data);
+            }
+        }
+
+        foreach (var data in AllTalentTypeDataList)
+        {
+            if (AllTalentTypeDataDic.ContainsKey(data.Type))
+            {
+                Debug.LogError("Find Same SkillID , SkillID= " + data.Type);
+            }
+            else
+            {
+                AllTalentTypeDataDic.Add(data.Type, data);
+            }
+        }
+
+    }
+
+
+    [XmlIgnore]
+    public Dictionary<int, Talents> AllTalentsDic = new Dictionary<int, Talents>();
+    [XmlIgnore]
+    public Dictionary<int, TalentTraitData> AllTalentTraitDataDic = new Dictionary<int, TalentTraitData>();
+    [XmlIgnore]
+    public Dictionary<int, TalentSkillData> AllTalentSkillDataDic = new Dictionary<int, TalentSkillData>();
+    [XmlIgnore]
+    public Dictionary<string, TalentTypeData> AllTalentTypeDataDic = new Dictionary<string, TalentTypeData>();
+
+    [XmlElement]
+    public List<Talents> AllTalentsList { get; set; }
+    [XmlElement]
+    public List<TalentTraitData> AllTalentTraitDataList { get; set; }
+    [XmlElement]
+    public List<TalentSkillData> AllTalentSkillDataList { get; set; }
+    [XmlElement]
+    public List<TalentTypeData> AllTalentTypeDataList { get; set; }
 }
 
 
 [System.Serializable] 
 public class Talents
 {
-
+    [XmlAttribute]
+    public int TalentID { get; set; }
+    [XmlAttribute]
+    public ushort TalentType { get; set; }
+    [XmlAttribute]
+    public string TalentName { get; set; }
+    [XmlAttribute]
+    public string TalentDesc { get; set; }
+    [XmlAttribute]
+    public string TalentIcon { get; set; }
+    [XmlAttribute]
+    public ushort LevelBase { get; set; }
+    [XmlAttribute]
+    public ushort LevelMax { get; set; }
+    [XmlAttribute]
+    public ushort TalentRarity { get; set; }
+    [XmlAttribute]
+    public string SkillBaseList { get; set; }
+    [XmlAttribute]
+    public string SkillExtendParam { get; set; }
+    [XmlAttribute]
+    public string TalentTagList { get; set; }
 }
 
 [System.Serializable] 
 public class TalentTraitData
 {
-
+    [XmlAttribute]
+    public int TraitID { get; set; }
+    [XmlAttribute]
+    public string TraitName { get; set; }
+    [XmlAttribute]
+    public string TraitDesc { get; set; }
+    [XmlAttribute]
+    public string TraitIcon { get; set; }
+    [XmlAttribute]
+    public string TraitEffect { get; set; }
 }
 
 [System.Serializable] 
 public class TalentSkillData
 {
-
+    [XmlAttribute]
+    public int SkillID { get; set; }
+    [XmlAttribute]
+    public ushort MaxLevel { get; set; }
+    [XmlAttribute]
+    public ushort SkillRarity { get; set; }
+    [XmlAttribute]
+    public string SkillName { get; set; }
+    [XmlAttribute]
+    public string SkillDesc { get; set; }
+    [XmlAttribute]
+    public string SkillIcon { get; set; }
+    [XmlAttribute]
+    public string SkillEffect { get; set; }
+    [XmlAttribute]
+    public float SkillGrowth { get; set; }
 }
 
 [System.Serializable] 
 public class TalentTypeData
 {
-
+    [XmlAttribute]
+    public string Type { get; set; }
+    [XmlAttribute]
+    public string TypeName { get; set; }
+    [XmlAttribute]
+    public string TypeDesc { get; set; }
+    [XmlAttribute]
+    public string TypeIcon { get; set; }
 }
--- GameMaster/RPGTale/Assets/Sim_FrameWork/Data/MetaData/TechnologyMetaData.cs
+++ GameMaster/RPGTale/Assets/Sim_FrameWork/Data/MetaData/TechnologyMetaData.cs
@@ -1,16 +1,118 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml.Serialization;
 
-public class TechnologyMetaData : MonoBehaviour {
+[System.Serializable]
+public class TechnologyMetaData : ExcelBase {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override void Construction()
+    {
+        AllTechnologyList = new List<Technology>();
+        for(int i = 0; i < 2; i++)
+        {
+            Technology te = new Technology();
+            te.TechnologyID = i;
+            te.TechType = "";
+            te.TechGetType = "";
+            te.TechName = "";
+            te.TechDesc = "";
+            te.TechIcon = "";
+            te.TechCostMaterialList = "";
+            te.TechCostUnitMin = "";
+            te.ResearchTime = i;
+            te.PreTechList = "";
+            AllTechnologyList.Add(te);
+        }
+
+        AllTechnologyTypeDataList = new List<TechnologyTypeData>();
+        for(int i = 0; i < 2; i++)
+        {
+            TechnologyTypeData tt = new TechnologyTypeData();
+            tt.Type = "";
+            tt.TypeName = "";
+            tt.TypeDesc = "";
+            tt.TypeIcon = "";
+            AllTechnologyTypeDataList.Add(tt);
+        }
+    }
+
+    public override void Init()
+    {
+        AllTechnologyDic.Clear();
+        AllTechnologyTypeDataDic.Clear();
+
+        foreach (var data in AllTechnologyList)
+        {
+            if (AllTechnologyDic.ContainsKey(data.TechnologyID))
+            {
+                Debug.LogError("Find Same TechnologyID , TechnologyID= " + data.TechnologyID);
+            }
+            else
+            {
+                AllTechnologyDic.Add(data.TechnologyID, data);
+            }
+        }
+
+        foreach (var data in AllTechnologyTypeDataList)
+        {
+            if (AllTechnologyTypeDataDic.ContainsKey(data.Type))
+            {
+                Debug.LogError("Find Same Type , Type= " + data.Type);
+            }
+            else
+            {
+                AllTechnologyTypeDataDic.Add(data.Type, data);
+            }
+        }
+    }
+
+
+    [XmlIgnore]
+    public Dictionary<int, Technology> AllTechnologyDic = new Dictionary<int, Technology>();
+    [XmlIgnore]
+    public Dictionary<string, TechnologyTypeData> AllTechnologyTypeDataDic = new Dictionary<string, TechnologyTypeData>();
+
+    [XmlElement]
+    public List<Technology> AllTechnologyList { get; set; }
+    [XmlElement]
+    public List<TechnologyTypeData> AllTechnologyTypeDataList { get; set; }
+}
+
+[System.Serializable] 
+public class Technology
+{
+    [XmlAttribute]
+    public int TechnologyID { get; set; }
+    [XmlAttribute]
+    public string TechType { get; set; }
+    [XmlAttribute]
+    public string TechGetType { get; set; }
+    [XmlAttribute]
+    public string TechName { get; set; }
+    [XmlAttribute]
+    public string TechDesc { get; set; }
+    [XmlAttribute]
+    public string TechIcon { get; set; }
+    [XmlAttribute]
+    public string TechCostMaterialList { get; set; }
+    [XmlAttribute]
+    public string TechCostUnitMin { get; set; }
+    [XmlAttribute]
+    public float ResearchTime { get; set; }
+    [XmlAttribute]
+    public string PreTechList { get; set; }
+}
+
+[System.Serializable] 
+public class TechnologyTypeData
+{
+    [XmlAttribute]
+    public string Type { get; set; }
+    [XmlAttribute]
+    public string TypeName { get; set; }
+    [XmlAttribute]
+    public string TypeDesc { get; set; }
+    [XmlAttribute]
+    public string TypeIcon { get; set; }
 }
