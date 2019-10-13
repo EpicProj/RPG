177fa74cd1749eafad233f4297c3cef6f7365ad1
SOOSOOC <1805790572@qq.com>
2019-10-13 12:24:29 +0800

    开始场景

---
+++ GameMaster/RPGTale/Assets/Sim_FrameWork/Project/ObjectMisc/Planet/PlanetRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sim_FrameWork
+{
+    public class PlanetRotate : MonoBehaviour
+    {
+        private Transform trans;
+        public float RotateSpeed = 0.0f;
+        void Start()
+        {
+            trans = this.transform;
+        }
+
+        void Update()
+        {
+            trans.Rotate(Vector3.up * RotateSpeed);
+        }
+    }
+}
